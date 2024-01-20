cf33a577be5682deea9c5c9678a24797cf3e0ee1
LukokiMakuntimaDaniel <lukokimakuntimadaniel@gmail.com>
2024-01-20 07:01:46 +0100

    criar estrutura da carteira digital

--- estruturas.h
+++ estruturas.h
@@ -1,5 +1,6 @@
 #ifndef FUNCOES_H_INCLUDED
 #define FUNCOES_H_INCLUDED
+#define MAX_CARTEIRA 2500
 
 struct DadosUser {
     char nome[50];
@@ -8,19 +9,46 @@ struct DadosUser {
     char curso[50];
     int idade;
     int ano;
-};
-
-struct carteira{
-    double saldo;
-    int numeroEstudante;
+    char senha[50];
+    char  numeroTelefone[50];
 };
 
 
-struct NOLDL {
+
+
+struct NOLDLU {
     struct DadosUser dadosUser;
     struct NOLDL* prev;
     struct NOLDL* next;
 };
 
+
+
+
+struct DadosCarteira{
+    double saldo;
+    char codigoCarteira[50];
+    int numeroEstudante;
+};
+
+struct CarteiraDigital {
+    struct DadosCarteira dadosCarteira[MAX_CARTEIRA];
+    int qtd;
+};
+
+struct dadosHistoricoTrasacao{
+    int ano;
+    int dia;
+    int mes;
+    int ano;
+
+
+};
+
+struct NOLDLHT {
+    struct carteira dadosCarteira;
+    struct NOLDL* prev;
+    struct NOLDL* next;
+};
 
 #endif // FUNCOES_H_INCLUDED
--- funcoes.h
+++ funcoes.h
@@ -3,8 +3,8 @@
 #include "estruturas.h"
 
 
-struct NOLDL* criarNoUser(struct DadosUser dadosUser) {
-    struct NOLDL* novoNo = (struct NOLDL*)malloc(sizeof(struct NOLDL));
+struct NOLDLU* criarNoUser(struct DadosUser dadosUser) {
+    struct NOLDLU* novoNo = (struct NOLDLU*)malloc(sizeof(struct NOLDLU));
     if (novoNo == NULL) {
         return 0;
     }
@@ -15,25 +15,26 @@ struct NOLDL* criarNoUser(struct DadosUser dadosUser) {
     return novoNo;
 }
 
-struct NOLDL* criarNoCarteira(struct DadosUser dadosUser) {
-    struct NOLDL* novoNo = (struct NOLDL*)malloc(sizeof(struct NOLDL));
+struct NOLDLC* criarNoCarteira(struct carteira dadosCarteira) {
+
+    struct NOLDLC* novoNo = (struct NOLDLC*)malloc(sizeof(struct NOLDLC));
     if (novoNo == NULL) {
         return 0;
     }
 
-    novoNo->dadosUser = dadosUser;
+    novoNo->dadosCarteira = dadosCarteira;
     novoNo->prev = NULL;
     novoNo->next = NULL;
     return novoNo;
 }
 
-void CadastrarUtilizador(struct NOLDL** cabeca, struct DadosUser dadosUser) {
-    struct NOLDL* novoNo = criarNoUser(dadosUser);
+void CadastrarUtilizador(struct NOLDLU** cabeca, struct DadosUser dadosUser) {
+    struct NOLDLU* novoNo = criarNoUser(dadosUser);
 
     if (*cabeca == NULL) {
         *cabeca = novoNo;
     } else {
-        struct NOLDL* temp = *cabeca;
+        struct NOLDLU* temp = *cabeca;
         while (temp->next != NULL) {
             temp = temp->next;
         }
@@ -42,6 +43,31 @@ void CadastrarUtilizador(struct NOLDL** cabeca, struct DadosUser dadosUser) {
     }
 }
 
+void cadastrarCarteira(struct NOLDLC** cabeca, struct carteira dadosUser) {
+    struct NOLDLC* novoNo = criarNoCarteira(dadosUser);
 
+    if (*cabeca == NULL) {
+        *cabeca = novoNo;
+    } else {
+        struct NOLDLC* temp = *cabeca;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = novoNo;
+        novoNo->prev = temp;
+    }
+}
+
+void adicionarDadosCarteira(struct CarteiraDigital* carteira, double saldo, const char* codigoCarteira, int numeroEstudante) {
+    if (carteira->qtd < MAX_CARTEIRA) {
+        carteira->dadosCarteira[carteira->qtd].saldo = saldo;
+        snprintf(carteira->dadosCarteira[carteira->qtd].codigoCarteira, sizeof(carteira->dadosCarteira[carteira->qtd].codigoCarteira), "%s", codigoCarteira);
+        carteira->dadosCarteira[carteira->qtd].numeroEstudante = numeroEstudante;
+        carteira->qtd++;
+        return 1;
+    } else {
+        return 0;
+    }
+}
 
 #endif // FUNCOES_H_INCLUDED
