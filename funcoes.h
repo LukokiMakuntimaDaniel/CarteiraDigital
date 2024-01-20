deb1e6b056b2bed46602d14ee1f218205c7a2fbb
LukokiMakuntimaDaniel <lukokimakuntimadaniel@gmail.com>
2024-01-20 08:15:47 +0100

    create a function cadastrarCarteiraEGravarArquivo

--- funcoes.h
+++ funcoes.h
@@ -64,7 +64,7 @@ void cadastratConta(struct NOLDLC** cabeca, struct dadosConta dados) {
 
 
 
-void CadastrarUtilizador(struct NOLDLU** cabeca, struct DadosUser dadosUser) {
+int CadastrarUtilizador(struct NOLDLU** cabeca, struct DadosUser dadosUser) {
     struct NOLDLU* novoNo = criarNoUser(dadosUser);
 
     if (*cabeca == NULL) {
@@ -101,24 +101,40 @@ void cadastrarTrasacao(struct NOLDLHT** cabeca, struct dadosHistoricoTrasacao da
 }
 
 
-void cadastrarCarteira(struct CarteiraDigital* carteira, double saldo, const char* codigoCarteira, int numeroEstudante) {
+int cadastrarCarteira(struct CarteiraDigital* carteira, double saldo, const char* codigoCarteira, int numeroEstudante) {
     if (carteira->qtd < MAX_CARTEIRA) {
         carteira->dadosCarteira[carteira->qtd].saldo = saldo;
         snprintf(carteira->dadosCarteira[carteira->qtd].codigoCarteira, sizeof(carteira->dadosCarteira[carteira->qtd].codigoCarteira), "%s", codigoCarteira);
         carteira->dadosCarteira[carteira->qtd].numeroEstudante = numeroEstudante;
         carteira->qtd++;
+    } else {
+        return 0;
+    }
+
+    if(cadastrarCarteiraEGravarArquivo(double saldo, const char* codigoCarteira, int numeroEstudante)){
+        return 1;
+    }else{
+        return 0;
+    }
+}
+
+int cadastrarUsuarioEGravarArquivo(struct DadosUser dadosUser) {
+    FILE *arquivoUsuario = fopen("usuarios.txt", "a");
+
+    if (arquivoUsuario != NULL) {
+        fprintf(arquivoUsuario, "%s,%d,%d,%s,%d,%d,%s,%s\n",dadosUser->nome, dadosUser->numeroEstudante, dadosUser->sala,dadosUser->curso, dadosUser->idade, dadosUser->ano,dadosUser->senha, dadosUser->numeroTelefone);
+        fclose(arquivoUsuario);
         return 1;
     } else {
         return 0;
     }
 }
 
-void cadastrarUsuarioEGravarArquivo(struct DadosUser dadosUser) {
-    FILE *arquivoUsuario = fopen("usuarios.txt", "a");
-
-    if (arquivoUsuario != NULL) {
-        fprintf(arquivoUsuario, "%s,%d,%d,%s,%d,%d,%s,%s\n",dadosUser->nome, dadosUser->numeroEstudante, dadosUser->sala,dadosUser->curso, dadosUser->idade, dadosUser->ano,dadosUser->senha, dadosUser->numeroTelefone);
-        fclose(arquivoUsuario);
+int cadastrarCarteiraEGravarArquivo(double saldo, const char* codigoCarteira, int numeroEstudante) {
+    FILE *arquivoCarteira = fopen("carteira.txt", "a");
+    if (arquivoCarteira != NULL) {
+        fprintf(arquivoCarteira, "%.2f,%s,%d\n",saldo,codigoCarteira,numeroEstudante);
+        fclose(arquivoCarteira);
         return 1;
     } else {
         return 0;
